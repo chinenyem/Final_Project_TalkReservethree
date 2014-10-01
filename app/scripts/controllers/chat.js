b22261405a546102c12e14d7c7e26a1383d8d4f6
Chinenyem Nwadiugwu <chnw1947@gmail.com>
2014-10-01 15:18:39 -0400

    added the volunteer feature

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -8,65 +8,14 @@
  */
 angular.module('talkreserveApp')
 .constant('FIREBASE_URI', 'https://talkreserve.firebaseio.com/')
-  .controller('ChatCtrl',  function ($scope, fbutil ) {
+  .controller('ChatCtrl',  function ($scope, fbutil) {
     
     
 
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
     // synchronize a read-only, synchronized array of messages, limit to most recent 10
-  $scope.selectedTopic = 0;
-  $scope.selctedGenre = {};
-  $scope.sermon = [
-    {
-      id: 0,
-      name: 'Spritual Empowerment',
-      lesson: [
-        'The Joy & Responsibility of Stewardship  1/3/2010 10AMa',
-        'The Joy & Responsibility of Stewardship  1/3/2010 10AMb',
-        'The Joy & Responsibility of Stewardship  1/3/2010 10AMc',
-        'The Joy & Responsibility of Stewardship  1/3/2010 10AMd'
-      ],
-
-      live: true
-    },
-    {
-      id: 1,
-      name: 'Family Empowerment',
-      lesson: [
-        'The Potential of Your Family 10/5/2010 7PMa',
-        'The Potential of Your Family 10/5/2010 7PMb',
-        'The Potential of Your Family 10/5/2010 7PMc',
-        'The Potential of Your Family 10/5/2010 7PMd'
-      ],
-      live: true
-    },
-    {
-      id: 2,
-      name: 'Finances',
-      lesson: [
-        'Having Faith In God\'s Covenant Blessing 12/16/2008 7:30PMa',
-        'Having Faith In God\'s Covenant Blessing 12/16/2008 7:30PMb',
-        'Having Faith In God\'s Covenant Blessing 12/16/2008 7:30PMc',
-        'Having Faith In God\'s Covenant Blessing 12/16/2008 7:30PMd'
-      ],
-      live: false
-    },
-    {
-      id: 3,
-      name: 'Marriage',
-      lesson: [
-        'Marriage Conference 2009 Lady Leisa Easley 11/14/2009a',
-        'Marriage Conference 2009 Lady Leisa Easley 11/14/2009b',
-        'Marriage Conference 2009 Lady Leisa Easley 11/14/2009c'
-      ],
-      live: true
-    }
-  ];
+  
 
     $scope.messages = fbutil.syncArray('messages', {limit: 20});
 
@@ -76,17 +25,30 @@ angular.module('talkreserveApp')
         
         //push a message to the end of the array
         $scope.messages.$add({
-          day: newMessage.day,
-          sermon: newMessage.selectedTopic,
-          name: newMessage.name, 
+          leader: newMessage.leader.volunteer,
+          ministry: newMessage.ministry, 
           email: newMessage.email,
-          price: newMessage.price,
-          payment: newMessage.payment,
-          paid: newMessage.paid
+          position: newMessage.position,
+          description: newMessage.description
           
         })
 
     };
+
+
+
+ $scope.addVolunteer = function(volunteer){
+    $scope.volunteers.$add({
+          leader: volunteer.message,
+          ministry: volunteer.message.ministry, 
+          email: volunteer.message.email,
+          position: volunteer.message.position,
+          description: volunteer.message.description
+
+    })
+   };
+
+
 
     //push a message to the end of the array
     $scope.deleteMessage = function(index){
@@ -94,9 +56,6 @@ angular.module('talkreserveApp')
       $scope.messages.$remove(index, 1);
     }
 
-
-
-    
 
  });
 
